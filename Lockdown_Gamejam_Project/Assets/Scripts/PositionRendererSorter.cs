88a18d3d26d5f9c5214e2e61d2e5e2b12be2f713
Maxim Eylenbosch <55977182+MaximEylenbosch@users.noreply.github.com>
2020-04-11 18:20:56 +0200

    UPDATED: Props, Characters prefabs

---
+++ Lockdown_Gamejam_Project/Assets/Scripts/PositionRendererSorter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionRendererSorter : MonoBehaviour
+{
+    [SerializeField] private int _sortingOrderBase = 5000;
+    [SerializeField] private int _offset = 0;
+    [SerializeField] private bool _runOnlyOnce = false;
+
+    private float _timer;
+    private float _timerMax = 0.1f;
+    private Renderer _renderer;
+
+    void Start()
+    {
+        _renderer = gameObject.GetComponent<Renderer>();
+    }
+
+    void LateUpdate()
+    {
+        _timer -= Time.deltaTime;
+        if (_timer <= 0f)
+        {
+            _timer = _timerMax;
+            _renderer.sortingOrder = (int)(_sortingOrderBase - transform.position.y - _offset);
+            if (_runOnlyOnce)
+            {
+                Destroy(this);
+            }
+        }
+    }
+}
